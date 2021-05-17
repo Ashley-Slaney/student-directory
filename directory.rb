ddadf3947dbca062cecd938cbf1d60a6c40582c4
Ashley Slaney <ashleyslaney96@gmail.com>
2021-05-17 16:04:43 +0000

    refactored print statement

--- directory.rb
+++ directory.rb
@@ -11,6 +11,5 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-print "Overall, we have "
-print 11
-puts " great students"
+student_count = 11
+print "Overall, we have #{student_count} great students"
