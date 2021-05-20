c6a3373314b979a202ab97fd0fd464e7a5f19beb
Ashley Slaney <ashleyslaney96@gmail.com>
2021-05-20 09:54:05 +0000

    save students to students.csv

--- exercises.rb
+++ exercises.rb
@@ -7,23 +7,26 @@ def interactive_menu
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
 def process(selection)
   case selection
     when "1"
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
       puts "I don't know what you mean, try again"
   end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
 end
 
 def show_students
@@ -122,6 +125,17 @@ def print_footer(students)
   else
     print "Overall, we have #{@students.count} great students".center(100)
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
